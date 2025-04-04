a1cde5e230224776a3ef2d2cec808b2ee35c2668
gauravz-git <gauravc0223@gmail.com>
2025-04-04 13:27:45 +0530

    Add division by zero check in simple calculator

--- simple_calculator.java
+++ simple_calculator.java
@@ -36,7 +36,9 @@ public class simple_calculator {
                 break;
             
             case '/':
-                result = num1 / num2;
+                if(num2 != 0){
+                    result = num1 / num2;
+                }
                 break;
         
             default:
